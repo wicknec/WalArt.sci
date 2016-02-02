3b28176178a1c076e654dc72fa37ef08141d0b6c
wicknec <wicknec@gmail.com>
2016-02-02 19:38:09 +0800

    __init__.py
    wicknec's library for scientific data processing in python

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+"""
+WalArt.sci
+=======================
+
+wicknec's library for scientific data processing in python
+
+"""
+# NOTE: the actual command documentation is collected from docstrings of the
+# commands and is appended to __doc__ after the class has been defined.
+"""
+Revisions
+=================
+160202 created
+"""
+__all__=['wiChart']
